87bc09ae965f56d12064738b7fdec301afbee7b3
Igor Mendes Gomes <igormendesgomesin1@outlook.com>
2026-06-08 22:55:56 -0300

    ThirdQuestion Answer

---
+++ ThirdQuestionPratice/cypress/e2e/carrinho.cy.js
@@ -0,0 +1,19 @@
+describe("Carrinho", () =>{
+    it('Adcionar produto ao carrinho com sucesso', () =>{
+
+        cy.visit('https://www.saucedemo.com')
+
+        
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
+
+        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+
+        cy.get('.shopping_cart_badge').should('be.visible').and('have.text', '1')
+
+        cy.get('#shopping_cart_container').click()
+        cy.get('[data-test="inventory-item-name"]').should('contain', 'Sauce Labs Backpack');
+    })
+    
+})
